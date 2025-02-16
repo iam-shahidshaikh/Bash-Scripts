93dc09ffa9c82c2ca06cc2adfaada5ecd06f6450
shahid13in <shahidshaikh13in@gmail.com>
2025-02-17 01:18:46 +0530

    add Arrays sh program

---
+++ Arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+Fruits=("apple" "mango" "Banana" "orange")
+echo "First Fruit is: ${Fruits[0]}"
+echo "Second Fruit is: ${Fruits[1]}"
+echo "Third Fruit is: ${Fruits[2]}"
+echo "Four Fruit is: ${Fruits[3]}"
